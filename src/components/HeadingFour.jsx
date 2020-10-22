05489fca3424b83e9fe1ff976944e668e33a696a
Soheil <s.tehrani1994@gmail.com>
2020-10-22 22:16:11 +0330

    completed features component

--- src/components/HeadingFour.jsx
+++ src/components/HeadingFour.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
 function HeadingFour({ children }) {
-  return <h4 className='heading-4'>{children}</h4>;
+  return <h4 className='heading-4 heading-4--dark'>{children}</h4>;
 }
 
 export default HeadingFour;
