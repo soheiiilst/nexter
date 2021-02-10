932df877c074121d9e10b1bb733d332960bf788c
Soheil <s.tehrani1994@gmail.com>
2021-02-10 23:09:28 +0330

    added footer component

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,11 +1,33 @@
-import React from 'react'
+import React from 'react';
 
 function Footer() {
   return (
     <footer className='footer'>
-      FOOTER
+      <ul className='nav'>
+        <li className='nav__item'>
+          <span className='nav__link'>Find your dream home</span>
+        </li>
+        <li className='nav__item'>
+          <span className='nav__link'>Request proposal</span>
+        </li>
+        <li className='nav__item'>
+          <span className='nav__link'>Download home planner</span>
+        </li>
+        <li className='nav__item'>
+          <span className='nav__link'>Contact us</span>
+        </li>
+        <li className='nav__item'>
+          <span className='nav__link'>Submit your property</span>
+        </li>
+        <li className='nav__item'>
+          <span className='nav__link'>Come work with us!</span>
+        </li>
+      </ul>
+      <p className="copyright">
+        &copy; Copyright {new Date().getFullYear()} by Nexter company.
+      </p>
     </footer>
-  )
+  );
 }
 
-export default Footer
+export default Footer;
