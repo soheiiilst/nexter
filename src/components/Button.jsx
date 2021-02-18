4bf4e98cb24cd70f78790d284b40f76d65619dbc
Soheil <s.tehrani1994@gmail.com>
2021-02-18 13:21:14 +0330

    added sidebar, header and heading-one components

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
-function Button({text}) {
-  return <button className='btn'>{text}</button>;
+function Button({text, cName}) {
+  return <button className={`btn ${cName ? cName : ''}`}>{text}</button>;
 }
 
 export default Button;
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,23 @@
 import React from 'react'
 
+import HeadingOne from './HeadingOne';
+import HeadingThree from './HeadingThree';
+import Button from './Button';
+
 function Header() {
   return (
     <header className='header'>
-      HEADER
+      <img src="img/logo-2.png" alt="Nexter logo" className='header__logo'/>
+      <HeadingThree>Your own home:</HeadingThree>
+      <HeadingOne>The ultimate personal freedom</HeadingOne>
+      <Button cName='header__btn' text='View our properties'/>
+      <div className="header__seenon-text">As seen on</div>
+      <div className="header__seenon-logos">
+        <img src="img/logo-bbc.png" alt="Seen on logo 1"/>
+        <img src="img/logo-forbes.png" alt="Seen on logo 2"/>
+        <img src="img/logo-techcrunch.png" alt="Seen on logo 3"/>
+        <img src="img/logo-bi.png" alt="Seen on logo 4"/>
+      </div>
     </header>
   )
 }
